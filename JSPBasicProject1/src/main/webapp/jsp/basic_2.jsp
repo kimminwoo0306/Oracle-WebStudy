59929f2fb3b3b133a9848a3de07d53d0cecc3a10
kimminwoo0306 <89583609+kimminwoo0306@users.noreply.github.com>
2023-01-06 18:08:53 +0900

    Add files via upload

---
+++ JSPBasicProject1/src/main/webapp/jsp/basic_2.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		for(int i=1;i<=10;i++)
+		{
+	%>
+		<b><%=i%></b><br>
+	<%		
+		}
+	%>
+	<c:forEach var="i" begin="1" end="10 step="1">
+	<b><${i }</b>
+	</c:forEach>
+</body>
+</html>
---
+++ JSPBasicProject2/src/main/java/com/sist/common/CreateConnection.java
@@ -0,0 +1,30 @@
+package com.sist.common;
+import java.util.*;
+import java.sql.*;
+public class CreateConnection {
+	private final String URL="jdbc:oracle:thin:@localhost:1521:xe";
+	public CreateConnection()
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch(Exception ex) {}
+	}
+	public Connection getConnection()
+	{
+		Connection conn=null;
+		try
+		{
+			conn=DriverManager.getConnection(URL,"hr","happy");
+		}catch(Exception ex) {}
+		return conn;
+	}
+	public void disConnection(PreparedStatement ps,Connection conn)
+	{
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		}catch(Exception ex) {}
+	}
+}
