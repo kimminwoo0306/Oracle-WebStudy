0dc00028696340ba4c57bb730a0a28068401a001
kimminou0306 <kimmi@DESKTOP-IG7P6LO>
2023-01-17 14:28:48 +0900

    23.01.17 답변게시판

--- JSPMiddleProject3/src/main/java/com/sist/dao/BoardDAO.java
+++ JSPMiddleProject3/src/main/java/com/sist/dao/BoardDAO.java
@@ -19,7 +19,7 @@ public class BoardDAO {
             Context c=(Context)init.lookup("java://comp/env");
             DataSource ds=(DataSource)c.lookup("jdbc/oracle"); // 이름으로 객체찾기
             conn=ds.getConnection();
-      } catch (Exception e) {}
+      } catch (Exception ex) {}
    }
    
    // 반환
@@ -28,7 +28,7 @@ public class BoardDAO {
          if(ps != null) ps.close();
          if(conn != null) conn.close();
          // POOL에 반환 (POOL : Connection객체가 저장된 메모리 공간)
-      } catch (Exception e) {}
+      } catch (Exception ex) {}
    }
    // 기능 => JSTL / EL
    // 1. 목록
@@ -60,25 +60,178 @@ public class BoardDAO {
          }
          rs.close();
          
-      } catch (Exception e) {
-         e.printStackTrace();
+      } catch (Exception ex) {
+         ex.printStackTrace();
       } finally {
          disConnection();
       }
       return list;
    }
-   public int boardRowCount()
+   public int boardRowCount() {
+      int count = 0;
+      try {
+         getConnection();
+         String sql = "SELECT COUNT(*) FROM replyBoard";
+         ps = conn.prepareStatement(sql);
+         ResultSet rs = ps.executeQuery();
+         rs.next();
+         count = rs.getInt(1);
+         rs.close();
+         
+      } catch (Exception ex) {
+         ex.printStackTrace();
+      } finally {
+         disConnection();
+      }
+      return count;
+   }
+   // 2. 새글
+   public void boardInsert(BoardVO vo) {
+      try {
+         getConnection();
+         String sql = "INSERT INTO replyBoard(no, name, subject, content, pwd, group_id) "
+               + "VALUES(rb_no_seq.nextval, ?, ?, ?, ?, (SELECT NVL(MAX(group_id)+1, 1) FROM replyBoard))";
+         ps = conn.prepareStatement(sql);
+         ps.setString(1, vo.getName());
+         ps.setString(2, vo.getSubject());
+         ps.setString(3, vo.getContent());
+         ps.setString(4, vo.getPwd());
+         
+         ps.executeUpdate();
+      } catch (Exception ex) {
+         ex.printStackTrace();
+      } finally {
+         disConnection();
+      }
+   }
+   // 3. 상세보기 => 매개변수 => ?뒤에 있는 값 => ? no=10
+   // JDBC => DBCP => ORM
+   public BoardVO boardDetailData(int no, int type)
    {
-	   int count=0;
+	   BoardVO vo=new BoardVO();
+	   try
+	   {
+		   getConnection(); // MyBatis, JPA
+		   String sql="";
+		   if(type==1) // Detail(조회수증가) => type=1 ,Update(증가안됨) type=2
+		   {
+			   sql="UPDATE replyBoard SET "
+				  +"hit=hit+1 "
+			      +"WHERE no=?";
+			   ps=conn.prepareStatement(sql);
+			   ps.setInt(1, no);
+			   ps.executeUpdate();
+		   }
+		   sql="SELECT no,name,subject,content,TO_CHAR(regdate,'YYYY-MM-DD'),hit "
+			  +"FROM replyBoard "
+			  +"WHERE no=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, no);
+		   ResultSet rs=ps.executeQuery();
+		   rs.next();
+		   vo.setNo(rs.getInt(1));
+		   vo.setName(rs.getString(2));
+		   vo.setSubject(rs.getString(3));
+		   vo.setContent(rs.getString(4));
+		   vo.setDbday(rs.getString(5));
+		   vo.setHit(rs.getInt(6));
+		   rs.close();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   disConnection(); 
+	   }
+	   return vo;
+   }
+   // 4. 답변 (*****) => SQL(4)
+   public void boardReplyInsert(int pno, BoardVO vo)
+   {
 	   try
 	   {
 		   getConnection();
-		   String sql="SELECT COUNT(*) FROM replyBoard";
+		   // 1. group_id, group_step, group_tab
+		   String sql="SELECT group_id, group_step, group_tab "
+				     +"FROM replyBoard "
+				     +"WHERE no=?";
 		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, pno);
 		   ResultSet rs=ps.executeQuery();
 		   rs.next();
-		   count=rs.getInt(1);
+		   BoardVO pvo=new BoardVO();
+		   pvo.setGroup_id(rs.getInt(1));
+		   pvo.setGroup_step(rs.getInt(2));
+		   pvo.setGroup_tab(rs.getInt(3));
 		   rs.close();
+		   // 2. group_step 조절
+		   sql="UPDATE replyBoard SET "
+			  +"group_step=group_step+1 "
+			  +"WHERE group_id=? AND group_step>?";	   
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1,	pvo.getGroup_id());
+		   ps.setInt(2, pvo.getGroup_step());
+		   ps.executeUpdate();
+		   // 3. insert
+		   sql="INSERT INTO replyBoard(no,name,subject,content,pwd,regdate,hit,group_id,group_step,group_tab, "
+			  +"root, depth) VALUES(rb_no_seq.nextval,?,?,?,?,SYSDATE,0,?,?,?,?,0)";
+		   ps=conn.prepareStatement(sql);
+		   ps.setString(1, vo.getName());
+		   ps.setString(2, vo.getSubject());
+		   ps.setString(3, vo.getContent());
+		   ps.setString(4, vo.getPwd());
+		   ps.setInt(5, pvo.getGroup_id());
+		   ps.setInt(6, pvo.getGroup_step()+1);
+		   ps.setInt(7, pvo.getGroup_tab()+1);
+		   ps.setInt(8, pno);
+		   ps.executeUpdate();
+		   // 4. depth증가
+		   sql="UPDATE replyBoard SET "
+			  +"depth=depth+1 "
+			  +"WHERE no=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, pno);
+		   ps.executeUpdate();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		 disConnection();  
+	   }
+   }
+   // 5. 수정
+   public boolean boardUpdate(BoardVO vo)
+   {
+	   boolean bCheck=false;
+	   try
+	   {
+		   getConnection();
+		   String sql="SELECT pwd FROM replyBoard "
+				     +"WHERE no=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, vo.getNo());
+		   ResultSet rs=ps.executeQuery();
+		   rs.next();
+		   String db_pwd=rs.getString(1);
+		   rs.close();
+		   
+		   if(db_pwd.equals(vo.getPwd()))
+	     	{
+			   bCheck=true;
+			   // 실제 수정
+			   sql="UPDATE replyBoard SET "
+				  +"name=?, subject=?, content=? "
+				  +"WHERE no=?";
+			   ps=conn.prepareStatement(sql);
+			   ps.setString(1, vo.getName());
+			   ps.setString(2, vo.getSubject());
+			   ps.setString(3, vo.getContent());
+			   ps.setInt(4, vo.getNo());
+			   ps.executeUpdate();
+		    }
 	   }catch(Exception ex)
 	   {
 		   ex.printStackTrace();
@@ -87,11 +240,75 @@ public class BoardDAO {
 	   {
 		   disConnection();
 	   }
-	   return count;
+	   return bCheck;
    }
-   // 2. 새글
-   // 3. 상세보기
-   // 4. 답변 (*****) => SQL(4)
-   // 5. 수정
    // 6. 삭제 => SQL(4)
+   public boolean boardDelete(int no, String pwd)
+   {
+	   boolean bCheck=false;
+	   try
+	   {
+		   getConnection();
+		   String sql="SELECT pwd,root,depth "
+				     +"FROM replyboard "
+				     +"WHERE no=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setInt(1, no);
+		   ResultSet rs=ps.executeQuery();
+		   rs.next();
+		   String db_pwd=rs.getString(1);
+		   int root=rs.getInt(2);
+		   int depth=rs.getInt(3);
+		   rs.close();
+		   
+		   if(db_pwd.equals(pwd))
+		   {
+			   bCheck=true;
+			   if(depth==0) // 답변이 없는 게시글 => 삭제
+			   {
+				   sql="DELETE FROM replyBoard "
+					  +"WHERE no=?";
+				   ps=conn.prepareStatement(sql);
+				   ps.setInt(1, no);
+				   ps.executeUpdate();
+			   }
+			   else // 수정 => 관리자가 삭제한 게시물
+			   {
+				   String msg="관리자가 삭제한 게시물입니다.";
+				   sql="UPDATE replyBoard SET "
+				      +"subject=?, content=? "
+					  +"WHERE no=?";
+				   ps=conn.prepareStatement(sql);
+				   ps.setString(1, msg);
+				   ps.setString(2, msg);
+				   ps.setInt(3, no);
+				   ps.executeUpdate();
+			   }
+			   //  depth 감소
+			   if(root!=0)
+			   {
+				   sql="UPDATE replyBoard SET "
+					  +"depth=depth-1 "
+					  +"WHERE no=?";
+				   ps=conn.prepareStatement(sql);
+				   ps.setInt(1, root);
+				   ps.executeUpdate();
+			   }
+		   }
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   disConnection();
+	   }
+	   return bCheck;
+   }
 }
+
+
+
+
+
+
--- JSPMiddleProject3/src/main/webapp/replyboard/list.jsp
+++ JSPMiddleProject3/src/main/webapp/replyboard/list.jsp
@@ -2,8 +2,82 @@
     pageEncoding="UTF-8" import="com.sist.model.*"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-
 <jsp:useBean id="model" class="com.sist.model.BoardModel"/>
+<%--
+	BoardModel model=new BoardModel()
+	출력 형식 : EL
+	${출력물} => 일반 변수가 아니다
+	           request.getAttribute("key") => request.setAttribute("key",value")
+	           ${requestScope.key}
+	           => 생략이 가능 => ${key}
+	           session. getAttribute("key") => session.setAttribute("key", "value")
+	           ${sessionScope.key}
+	     연산자
+	        산술연산자 => 문자열 결합 (+=)
+	                    null연산 => null은 0이다
+	                    ${null+10} => 10
+	                    ${"10"+10} => 20
+	                    ${"hello"+" el"} => 오류
+	                    ----------------
+	                    ${"hello"+=" el"} => hello el
+	                    정수/정수 = 실수
+	                    
+	                    / => div ${10/3}, ${10 div 3}
+	                    % => mod ${10%3}, ${10 mod 3}
+	        비교연산자 => 결과값 (true/false) => 문자열, 날짜도 동일하게 처리
+	                    == eq
+	                    != ne
+	                    <  lt
+	                    >  gt
+	                    <= le
+	                    >= ge
+	        논리연산자 => && and , || or, not()
+	        ------------------------------------ <c:if test="${비교연산,논리연산}">
+	        삼항연산자 => 조건 ? 값1 : 값2
+	        
+	        JSTL => JSP안에서 java코딩을 최소화 (자바제어문 => 태그형으로 제작)
+	        core =>
+	                <c:set> : 변수 설정
+	                     <c:set var="msg" value="Hello JSTL">
+	                     => request.setAttribute("msg","Hello JSTL");
+	                <c:out> : 화면 출력
+	                     <c:out value="Hello"> => out.println("Hello") => JQuery, VueJS.. 자바스크립트에서 주로 사용
+	                     
+	                <c:forEach> : for문
+	                     <c:forEach var="i" begin="1" end="10" step="1">
+	                        => for(int i=1; i<=10; i++)
+	                     <c:forEach var="vo" items="${list}"> => 가장 많이 사용 -> for-each(향상된  for문)
+	                     => for(VO vo:list)
+	                     <% %> <%= %> => 사용하지 않는다 (스프링) 
+	                <c:choose>
+	                     <c:choose>
+	                     	<c:when test=""></c:when>
+	                     	<c:when test=""></c:when>
+	                     	<c:when test=""></c:when>
+	                     	<c:when test=""></c:when>
+	                     	<c:when test=""></c:when>
+	                     	<c:otherwise></c:otherwise> else
+	                     </c:choose>
+	                  <c:if> == XML
+	                        <c:if test="">
+	                        	처리문장
+	                        </c:if>
+	                        DTD => 태그, 속성 정의 => XML의 단점 : 태그에 순서 (MyBatis, Spring) => XML기반
+	                        => XML 파싱 => JAXP, JAXB
+	                                      DOM/SAX
+	                        => JSON 파싱 => REST
+	                <c:forTokens> : StringTokenizer
+	        format => Oracle SQL문장에서 처리
+	            <fmt:formatDate>
+	            <fmt:formatNumber>
+	        fn => Model에서 처리 => 메소드 String
+	        ------------------ 보안의 문제가 있다 (거의 사용하지 않는다)
+	        sql => DAO, ORM
+	        xml => OXM
+	        ------------------
+	        
+	        <c:forEach>, <c:if>, <c:choose>, <c:redirect> => 대소문자 구분
+ --%>
 
 <%
     model.boardListData(request, response);
@@ -30,14 +104,14 @@
 <body>
    <div class="container">
      <div class="row">
-       <img src="qna.jpg" style="width: 200px; height=50px;">
+       <img src="qna.jpg" style="width: 800px; height:150px" class="img-rounded">
      </div>
      <div style="height: 5px"></div>
      <div class="row">
        <table class="table">
          <tr>
            <td>
-             <a href="#" class="btn btn-sm btn-danger">새글</a>
+             <a href="insert.jsp" class="btn btn-sm btn-danger">새글</a>
            </td>
          </tr>
        </table>
@@ -53,13 +127,18 @@
            <tr class="warning">
              <td width="10%" class="text-center">${vo.no }</td>
              <td width="45%">
-             <c:if test="${vo.group_tab >0}">>
+             <c:if test="${vo.group_tab >0}">
                <c:forEach var="i" begin="0" end="${vo.group_tab }">
-                 &nbsp;&nbsp;
+               &nbsp;&nbsp;
                </c:forEach>
                <img src="re_icon.png">
              </c:if>
-             ${vo.subject }
+             <c:if test="${vo.subject==msg }">
+             	<span style="color:gray">${vo.subject }</span>
+             	</c:if>
+             	<c:if test="${vo.subject!=msg }">
+             	<a href="detail.jsp?no=${vo.no}">${vo.subject }
+             </c:if>
              &nbsp;
              <c:if test="${today==vo.dbday }">
                <sup><img src="new.gif"></sup>
